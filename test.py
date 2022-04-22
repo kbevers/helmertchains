73ab79ebfbb3d5099db51d32fc4d8a4bd042a5b8
Kristian Evers <kristianevers@gmail.com>
2022-04-22 22:26:49 +0200

    Fix hastily rewritten test-suite

--- test.py
+++ test.py
@@ -64,13 +64,13 @@ def test_two_consecutive_helmerts(H1, H2, coord):
     assert is_vector_close(c2, c3), "Math verification failed!"
 
 
-def test_same_results_as_proj(H1, H2, coord):
+def test_same_results_as_proj(H1, H2, H3, coord):
     """Verify transformation results against PROJ"""
     pipeline = f"""
         +proj=pipeline +step
         +proj=helmert +x={H1.x} +y={H1.y} +z={H1.z}
-                    +rx={H1.rx} +ry={H1.ry} +rz={H1.rz}
-                    +s={H1.s}  +convention=position_vector
+                      +rx={H1.rx} +ry={H1.ry} +rz={H1.rz}
+                      +s={H1.s}  +convention=position_vector
                     +step
         +proj=helmert +x={H2.x} +y={H2.y} +z={H2.z}
                     +rx={H2.rx} +ry={H2.ry} +rz={H2.rz}
@@ -79,8 +79,10 @@ def test_same_results_as_proj(H1, H2, coord):
     helmert = pyproj.transformer.Transformer.from_pipeline(pipeline)
     c = np.array(helmert.transform(coord[0], coord[1], coord[2]))
 
+    c3 = H3.transform(coord)
+
     # Verify that the results are the same, if they are my math is the same as PROJ's
-    is_vector_close(coord, c), "PROJ verification failed!"
+    assert is_vector_close(c, c3), "PROJ verification failed!"
 
 
 def test_helmert_commutativeness(H1, H2, H3, coord):
@@ -91,7 +93,7 @@ def test_helmert_commutativeness(H1, H2, H3, coord):
     c1 = H4.transform(coord)
     c2 = H5.transform(coord)
     # Verify that Helmerts commute(?)
-    is_vector_close(c1, c2), "Commutative property verification failed!"
+    assert is_vector_close(c1, c2), "Commutative property verification failed!"
 
 
 def test_inverse_transform(H1, coord):
@@ -100,4 +102,5 @@ def test_inverse_transform(H1, coord):
     c2 = H1.transform(c1, inverse=True)
 
     # Verify that the inverse transform works
-    is_vector_close(coord, c2), "Inverse property verification failed!"
+    assert is_vector_close(coord, c2), "Inverse property verification failed!"
+
